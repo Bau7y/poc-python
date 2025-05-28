9bf762ed49df1c1f25faec5bc53f954fd08aed6b
Sebas <sebasrojash08@gmail.com>
2025-05-28 09:40:11 -0600

    feat: PromedioEstEj

--- promedioEst.py
+++ promedioEst.py
@@ -1,4 +1,22 @@
+def calcProm(notas):
+    for index in notas:
+        nota = index+index+index
+    return nota/3
+        
+
+def reqProms():
+    listaNotas = []
+    for i in range(0, 3):
+        user = int(input("Digite las tres notas del estudiante: "))
+        listaNotas.append(user)
+    return listaNotas
+
+
+def main():
+    listaNotas = reqProms()
+    promEst = calcProm(listaNotas)
+    print("El promedio del estudiante es de ", promEst )
 
 
 if __name__ == '__main__':
-    pass
+    main()
