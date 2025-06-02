69888cefe5bedca634dfe97758e948881e9a20d2
Sebas <sebasrojash08@gmail.com>
2025-06-01 21:49:27 -0600

    feat: loopFix

--- equipos.py
+++ equipos.py
@@ -1,29 +1,34 @@
-def calcMatches(points):
-        wMatches = int(input("Digite el numero de partidos ganados: "))
-        return points.insert(0, wMatches * 3)
-        #tMatches = int(input("Digite el numero de partidos empatados: "))
-        #return points.insert(1, tMatches * 2 + points[1])
-        #lMatches = int(input("Digite el numero de partidos empatados: "))
-        #return points.insert(2, lMatches  + points[2])
+def calcMatches(points, user):
+        match user:
+            case 1:
+                wMatches = int(input("Digite el numero de partidos ganados: "))
+                return points.insert(0, wMatches * 3)
+            case 2:
+                tMatches = int(input("Digite el numero de partidos empatados: "))
+                return points.insert(1, tMatches * 2 )
+            case 3:
+                lMatches = int(input("Digite el numero de partidos perdidos: "))
+                return points.insert(2, lMatches)
+            
 
 def reqMatchesW(points):
-    userOpt = int(input("Digite 1 para digitar los partidos ganados, 2 para los empatados, 3 para los perdidos y 4 para salir "))
+    userOpt = 0
+    listaPuntos = []
     while (userOpt != 4):
+        userOpt = int(input("Digite 1 para digitar los partidos ganados, 2 para los empatados, 3 para los perdidos y 4 para salir "))
         match userOpt:
             case 1:
-                listaPuntos = calcMatches(points)
-                
+                listaPuntos = calcMatches(points, userOpt)
+                print(listaPuntos)
             case 2:
                 calcMatches(points, userOpt)
             case 3:
                 calcMatches(points, userOpt)
+            case 4:
+                print("Saliendo...")
             case _:
                 print("La opcion digitada no es valida...")
-                continue;
-    else:
-        pass
     
-
 
 def main():
     points = []
