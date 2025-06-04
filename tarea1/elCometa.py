2271edde1d8db7ec00ab95f33063ece2655991ea
Sebas <sebasrojash08@gmail.com>
2025-06-03 18:39:44 -0600

    feat: tarea1 precioVenta

--- tarea1/elCometa.py
+++ tarea1/elCometa.py
@@ -1,10 +1,37 @@
 import random
 
+def showSalePrice(rawMat, labor, manufactCost, key):
+    print("La clave de su producto es: ", key)
+    print("El precio de venta final es de: ", rawMat + labor + manufactCost)
+
+
+def calcManufactCost(rawMat, prodKey):
+    if (prodKey == 2) | (prodKey == 5):
+        return rawMat * 0.30;
+    if (prodKey == 3) | (prodKey == 6):
+        return rawMat * 0.35;
+    if (prodKey == 1) | (prodKey == 4):
+        return rawMat * 0.28;
+
+def calcLabor(rawMat, prodKey):
+    if (prodKey == 3) | (prodKey == 4):
+        return rawMat * 0.75;
+    if (prodKey == 1) | (prodKey == 5):
+        return rawMat * 0.80;
+    if (prodKey == 2) | (prodKey == 6):
+        return rawMat * 0.85;
+
+
+def reqData():
+    return int(input("Digite la materia prima total del producto: "))
+
 
 def main():
-    listaClaves = [1, 2, 3, 4, 5, 6]
-    numClave = random(1,6)
-
+    key = random.randrange(1, 6)
+    rawMaterial = reqData()
+    labor = calcLabor(rawMaterial, key)
+    manufactCost = calcManufactCost(rawMaterial, key)
+    showSalePrice(rawMaterial, labor, manufactCost, key)
 
 if __name__ == "__main__":
     main()
