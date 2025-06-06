b7db5af4f0a909e33a883794aaf6f4b382d512f1
Sebas <sebasrojash08@gmail.com>
2025-06-06 12:05:25 -0600

    feat: tarea1Pt2 loan

--- tarea1Pt2/MDI.py
+++ tarea1Pt2/MDI.py
@@ -1,5 +1,49 @@
 import random
 
+def showData(user):
+    for key, data in user.items():
+        print(f"{key}, {data}")
+
+def reqData():
+    user = {
+        "nombre" : "",
+        "dineroSolicitado" : 0,
+        "salarioAnual" : 0,
+        "valorPropiedades" : 0
+    }
+    try:
+        user["nombre"] = input("Digite el nombre del solicitante: ")
+        user["dineroSolicitado"] = int(input("Cuánto dinero se está siendo solicitado? : "))
+        user["salarioAnual"] = int(input("Digite el salario anual del solicitante: "))
+        user["valorPropiedades"] = int(input("Digite el valor total de las propiedades del solicitante: "))
+        return user;
+    except:
+        print("Solo se admiten números...")
+
+def reqOptn():
+    print("\nDigite 1 para solicitar el préstamo\nDigite 2 para mostrar los datos del usuario\nDigite 3 para salir")
+    try:
+        return int(input("Opción: "))
+    except ValueError:
+        print("Solo se permite digitar números...")
+
+
+def loanAsking():
+    userOptn = 0
+    while (userOptn != 3):
+        userOptn = reqOptn()
+        match userOptn:
+            case 1:
+                newUser = reqData()
+            case 2:
+                try:
+                    showData(newUser)
+                except UnboundLocalError:
+                    print("\nUsuario sin registrar...")
+            case _:
+                print("La opción digitada no existe...")
+    print("\nSaliendo...\n")
+
  #///////////////////////////////////////////Programa Adivinar el numero/////////////////////////////////
 def reqNumber():
     try:
@@ -13,13 +57,13 @@ def guessing(rndm):
     while(userTry < 5):
         user = reqNumber()
         if (user < rndm):
-            print(">")
+            print("\n>\n")
             userTry +=1
         if (user > rndm):
-            print("<")
+            print("\n<\n")
             userTry +=1
         if (user == rndm):
-            print("Felicidades has adivinado")
+            print("Felicidades has adivinado\n")
             break;
         if (userTry == 5):
             print("Lo siento, más suerte para la próxima")
@@ -32,7 +76,7 @@ def guessNumber():
 
 
 def option():
-    print("digite 1 para el programa de adivinar el numero\ndigite 2 para el programa de préstamos\ndigite 3 para el programa de maquina expendedora\ndigite 4 para salir")
+    print("digite 1 para el programa de adivinar el numero\ndigite 2 para el programa de préstamos\ndigite 3 para el programa de maquina expendedora\ndigite 4 para salir\n")
     try:
         return int(input("Opción: "));
     except ValueError:
@@ -49,7 +93,7 @@ def menuHandler():
             case 1:
                 guessNumber()
             case 2: 
-                pass
+                loanAsking()
             case 3:
                 pass
             case 4:
