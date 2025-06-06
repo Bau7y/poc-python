f127d5516f26f3e61afae944bbdaa6d12ab80996
Sebas <sebasrojash08@gmail.com>
2025-06-05 21:20:35 -0600

    feat: tarea1pt2 guessNumb

--- tarea1Pt2/MDI.py
+++ tarea1Pt2/MDI.py
@@ -1,12 +1,44 @@
+import random
+
+ #///////////////////////////////////////////Programa Adivinar el numero/////////////////////////////////
+def reqNumber():
+    try:
+        return int(input("Digite su numero: "))
+    except ValueError:
+        print("Solo se admiten numeros")
+
+
+def guessing(rndm):
+    userTry = 0
+    while(userTry < 5):
+        user = reqNumber()
+        if (user < rndm):
+            print(">")
+            userTry +=1
+        if (user > rndm):
+            print("<")
+            userTry +=1
+        if (user == rndm):
+            print("Felicidades has adivinado")
+            break;
+        if (userTry == 5):
+            print("Lo siento, más suerte para la próxima")
+    
+
+
+def guessNumber():
+    rndmNumber = random.randint(1,10)
+    guessing(rndmNumber)
+
+
 def option():
-    print("digite 1 para el programa de adivinar el numero")
-    print("digite 2 para el programa de préstamos")
-    print("digite 3 para el programa de maquina expendedora")
-    print("digite 4 para salir...")
+    print("digite 1 para el programa de adivinar el numero\ndigite 2 para el programa de préstamos\ndigite 3 para el programa de maquina expendedora\ndigite 4 para salir")
     try:
         return int(input("Opción: "));
     except ValueError:
         print("Solo se permite digitar números...")
+
+#////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 def menuHandler():
@@ -15,7 +47,7 @@ def menuHandler():
         userOpt = option()
         match userOpt:
             case 1:
-                pass
+                guessNumber()
             case 2: 
                 pass
             case 3:
@@ -25,7 +57,6 @@ def menuHandler():
             case _:
                 print("No se ha encontrado la opción digitada...")
             
-
 
 if __name__ == "__main__":
     menuHandler()
