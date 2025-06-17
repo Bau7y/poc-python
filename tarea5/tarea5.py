dcf6e6ef8021695e31ba88921842b0036c626981
Sebas <sebasrojash08@gmail.com>
2025-06-17 16:26:11 -0600

    feat: reports

--- tarea5/tarea5.py
+++ tarea5/tarea5.py
@@ -1,3 +1,18 @@
+
+
+def reporte(sucursales):
+    dicReportes = {}
+    listaClientes = []
+    for sucursal in sucursales:
+        dicReportes["ganancias_netas"] = sucursal["ventas"] - sucursal["gastos"] + dicReportes.get("ganancias_netas", 0)
+        dicReportes["horas_trabajadas"] = sucursal["horas"] + dicReportes.get("horas_trabajadas", 0)
+        listaClientes.append(sucursal["clientes"])
+        listaClientes.sort(reverse=True)
+        dicReportes["top_clientes"] = sucursal["nombre"] if sucursal["clientes"] == listaClientes[0] else dicReportes.get("top_clientes", "N/A")
+        dicReportes["menos_cliente"] = sucursal["nombre"] if sucursal["clientes"] == listaClientes[-1] else dicReportes.get("menos_cliente", "N/A")
+    return dicReportes;
+
+
 def reqUserOpt():
     print("1.Reporte\n2.Personas\n3.Divisas\n4.Salir\n")
     try:
@@ -7,11 +22,18 @@ def reqUserOpt():
 
 
 def menuHandler():
+    sucursales = [{"nombre":"San José", "ventas":105, "clientes":30, "horas":90, "gastos":20},
+              {"nombre":"Cartago", "ventas":130, "clientes":73, "horas":60, "gastos":18},
+              {"nombre":"Heredia", "ventas":150, "clientes":45, "horas":85, "gastos":34},
+              {"nombre":"Alajuela", "ventas":180, "clientes":18, "horas":45, "gastos":15},
+              {"nombre":"Puntarenas", "ventas":95, "clientes":26, "horas":65, "gastos":41},
+              {"nombre":"Guanacaste", "ventas":122, "clientes":20, "horas":18, "gastos":23},
+              {"nombre":"Limon", "ventas":125, "clientes":89, "horas":38, "gastos":16}]
     while(True):
         userOpt = reqUserOpt()
         match userOpt:
             case 1:
-                print("Reporte")
+                print(reporte(sucursales))
             case 2:
                 print("Personas")
             case 3:
