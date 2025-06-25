d7ca9585268868633573f7d65797c863e9389f8e
Sebas <sebasrojash08@gmail.com>
2025-06-24 18:50:10 -0600

    feat: card selection

--- lab01/lab01.py
+++ lab01/lab01.py
@@ -1,22 +1,38 @@
 import random, os
 from Humano import Humano
 
-def showCards():
-    mazo = Humano().getCartas()
+def reqCard(mazo):
+    while(True):
+        try:
+            option = input("\nSeleccione una carta(# de ????): ")
+            for key, val in mazo.items():
+                if option == key:
+                    selectedCard = [key, val]
+                    print("\nCarta seleccionada: ", selectedCard)
+                    return selectedCard
+            print("\nNo se ha encontrado la carta solicitada\n")
+        except:
+            print("Solo se admiten numeros...")
+
+
+def showCards(mazo):
     carta=0
     while(carta < 5):
         for key, value in mazo.items():
             carta +=1
             print(f"{carta}. ",key, value)
 
+
 def game():
     ronda = 0
     while(ronda < 5):
         listaReglas = ["conocimiento + estrategia", "estrategia + energía", "(conocimiento * 2) - energía", "conocimiento + estrategia + energía", "estrategia * energía"]
         index = random.randint(0,4)
+        mazo = Humano().getCartas()
         os.system("cls")
-        print("---------------Juego---------------\nRegla de la ronda: ", listaReglas[index], "\n\n")
-        showCards()
+        print("---------------Humano vs IA---------------\nRegla de la ronda: ", listaReglas[index], "\n\n")
+        showCards(mazo)
+        card = reqCard(mazo)
         os.system("pause")
         ronda += 1
 
@@ -40,7 +56,6 @@ def mnuHandler():
                 quit()
             case _:
                 print("No se ha encontrado la opción digitada...\n")
-
 
 
 if __name__ == "__main__":
