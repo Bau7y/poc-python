6704384fd3e54014ed74167451e694020f439153
Sebas <sebasrojash08@gmail.com>
2025-07-03 18:29:17 -0600

    feat: mnu add

--- tarea8/tarea8.py
+++ tarea8/tarea8.py
@@ -1,6 +1,27 @@
 
+def reqUserOpt():
+    print("1. Generar Matriz de temperaturas\n2. Ver resultados\n3. Salir")
+    try:
+        return int(input("\nIngrese una opcion: "))
+    except ValueError:
+        print("Solo se admiten números...")
+    
+
+
 def mnuHandler():
-    pass
+    matrizGrados = []
+    while(True):
+        userOpt = reqUserOpt()
+        match userOpt:
+            case 1:
+                pass
+            case 2:
+                pass
+            case 3:
+                print("saliendo...")
+                quit()
+            case _:
+                print("Opcion invalida")
 
 
 if __name__ == "__main__":
