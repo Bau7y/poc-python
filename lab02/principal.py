92d57aebeae60aaf5eed0f644fb43319cada21cf
Sebas <sebasrojash08@gmail.com>
2025-07-16 19:01:56 -0600

    feat: si

--- lab02/principal.py
+++ lab02/principal.py
@@ -2,14 +2,26 @@ import tabulate
 import random
 import os
 from colorama import Fore, Style, init
+import copy
 
-def calcularCamino(matriz, puntos, cordenadas, row):
+
+def showStats(stats, matriz):
+    for key, val in stats.items():
+        print(Fore.RED + f"{key}: {val}" + Style.RESET_ALL)
+    print(tabulate.tabulate(matriz, tablefmt="fancy_grid"))
+
+
+def calcularCamino(matriz, puntos, cordenadas, row, dire, diag):
     os.system("cls")
     print(tabulate.tabulate(matriz, tablefmt="fancy_grid"))
     pos = random.randint(0, row-1)
-    for f in range(len(matriz)):
-        for c in range(len(matriz[f])):
-            print(f"Actualmente se encuentra en la posición {pos},{0}")
+    print(Fore.GREEN + f"Su posición de inicio es {pos}, {0}")
+    os.system("pause")
+    stats = {"puntos": puntos, "cordenadas": cordenadas}
+    while(stats["puntos"] < 40):
+        for f in range(len(matriz)):
+            for c in range(len(matriz[f])):
+                pass
 
 
 def llenarCamino(row, col):
@@ -17,7 +29,7 @@ def llenarCamino(row, col):
     for i in range(row):
         matriz.append([])
         for j in range(col):
-            if j < col//2:
+            if j < col // 2:
                 matriz[i].append(random.randint(1, 15))
             else:
                 if j < int(col*0.75):
@@ -37,11 +49,15 @@ def  solicitarNums():
 def main():
     puntos = 100
     cordenadas = []
+    direcciones = [(-1,0),(1,0),(0,-1),(0,1)]
+    direccionesDiag = [(-1, -1),(-1, 1),(1, -1),(1, 1)]
     row, col = solicitarNums()
     matri = llenarCamino(row, col)
     print(tabulate.tabulate(matri, tablefmt="fancy_grid"))
     os.system("pause")
-    stats = calcularCamino(matri, puntos, cordenadas, row)
+    stats, matriz = calcularCamino(matri, puntos, cordenadas, row, direcciones, direccionesDiag)
+    os.system("cls")
+    showStats(stats, matriz)
 
 
 
