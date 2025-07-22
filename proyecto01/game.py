24cfebde3d31f02c83a0ca55e9465d490038f389
Sebas <sebasrojash08@gmail.com>
2025-07-22 14:08:46 -0600

    feat: class Add

--- proyecto01/game.py
+++ proyecto01/game.py
@@ -1,33 +1,48 @@
 import pygame
 from sys import exit
-
+from components import Button
 
 pygame.init()
-
 tk = pygame.display.set_mode((1200, 800))
 pygame.display.set_caption("Flood")
-icon = pygame.image.load("proyecto01\images\icon.png")
-pygame.display.set_icon(icon)
-clock = pygame.time.Clock()
 
-frameSurface = pygame.image.load("proyecto01\images\menuFrame2.png") 
-frameSurface = pygame.transform.scale(frameSurface, (1200, 800))
-bg = frameSurface.get_at((0, 0))
-frameSurface.set_colorkey(bg)
+def main_menu():
+    icon = pygame.image.load("proyecto01\images\icon.png")
+    pygame.display.set_icon(icon)
+    clock = pygame.time.Clock()
 
-scaleStart = pygame.image.load("proyecto01\images\start.png")
-scaleStart = pygame.transform.scale(scaleStart, (350, 350))
-bgStart = scaleStart.get_at((0, 0))
-scaleStart.set_colorkey(bgStart)
+    frameSurface = pygame.image.load("proyecto01\images\menuFrame.png") 
+    frameSurface = pygame.transform.scale(frameSurface, (1200, 800))
+    bg = frameSurface.get_at((0, 0))
+    frameSurface.set_colorkey(bg)
+
+    scaleStart = pygame.image.load("proyecto01\images\start.png")
+    scaleStart = pygame.transform.scale(scaleStart, (350, 350))
+    bgStart = scaleStart.get_at((0, 0))
+    scaleStart.set_colorkey(bgStart)
+
+    scaleQuit = pygame.image.load("proyecto01\images\quitScale.png")
+    scaleQuit = pygame.transform.scale(scaleQuit, (250, 250))
+    bgQuit = scaleQuit.get_at((0, 0))
+    scaleQuit.set_colorkey(bgQuit)
 
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit()
-    tk.blit(frameSurface, (55, 7))
-    tk.blit(scaleStart, (428, 100))
+    while True:
+        mousePos = pygame.mouse.get_pos()
+        
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+        tk.blit(frameSurface, (55, 7))
+        tk.blit(scaleStart, (428, 100))
+        tk.blit(scaleQuit, (522, 300))
 
-    pygame.display.update()
-    clock.tick(60) #fps
+        pygame.display.update()
+        clock.tick(60) #fps
+
+
+if __name__ == "__main__":
+    main_menu()
+
     
