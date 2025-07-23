cbb2ca9c99fc585a050cc7278a4032552e7da4ae
Sebas <sebasrojash08@gmail.com>
2025-07-23 02:12:55 -0600

    feat: button class act

--- proyecto01/components/Button.py
+++ proyecto01/components/Button.py
@@ -1,16 +1,6 @@
-class Button:
-    def __init__(self, image, pos, text, font, base_color, hover_color):
-        self.image = image
-        self.x_pos = pos[0]
-        self.y_pos = pos[1]
-        self.font = font
-        self.base_color, self.hover_color = base_color, hover_color
-        self.text_input = text
-        self.text = self.font.render(self.text_input, True, self.base_color)
-        if self.image is None:
-            self.image = self.text
-        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
-        self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
+class Buttons:
+    def __init__(self, x, y, image, image_hover, scale=1):
+        pass
     
     def update(self, screen):
         if self.image is not None:
