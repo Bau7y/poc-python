a9b41b5336d5efe3cacbc84aba872cd350b8ec0d
Sebas <sebasrojash08@gmail.com>
2025-08-03 23:21:47 -0600

    feat: class mesa

---
+++ proyecto01/components/Mesa.py
@@ -0,0 +1,22 @@
+import pygame
+
+class MesaInteractuable:
+    def __init__(self, rect, objetos):
+        self.rect = rect
+        self.objetos = objetos
+        self.visible = True
+        self.enabled = True
+
+
+    def try_interactTable(self, player):
+        if self.enabled:
+            player.estado = "mesa"
+            player.mesa_abierta = self
+
+
+class ObjetoInteractivo:
+    def __init__(self, imagen, nombre, valor, pos_ui):
+        self.imagen = imagen
+        self.nombre = nombre
+        self.valor = valor
+        self.rect = self.imagen.get_rect(topleft=pos_ui)
