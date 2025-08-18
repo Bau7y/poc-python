43dd3f5c8b221f885dab3b82ca7c67331d73f41d
Sebas <sebasrojash08@gmail.com>
2025-08-18 13:09:32 -0600

    feat: db conn done

--- proyecto02/DataBaseConnection.py
+++ proyecto02/DataBaseConnection.py
@@ -1,11 +1,15 @@
 from pyodbc import *
 from Persona import *
+import os
 
 class DBConnection:
     def __init__(self):
-        self.driver = r"Driver={Microsoft Access Driver (*.mdb, *.accdb)};"
-        self.DBPath = r"Dbq=DB/BasePersonas.accdb;"
-        self.conn = connect(self.driver+self.DBPath)
+        self.driver =r"Driver={Microsoft Access Driver (*.mdb, *.accdb)};"
+        baseDir = os.path.dirname(os.path.abspath(__file__))
+        dbFile = os.path.join(baseDir, "DB", "BasePersonas.accdb")
+        self.DBPath = rf"Dbq={dbFile};"
+
+        self.conn = connect(self.driver + self.DBPath)
         self.cursor = self.conn.cursor()
 
     def closeConnection(self):
@@ -17,6 +21,8 @@ class DBConnection:
         data = self.cursor.fetchall()
         listaPersonas = []
         for row in data:
-            persona = Persona(personId=row[0], name=row[1])
+            persona = Persona(personId=row[0], name=row[1], birthDate=row[2], deathDate=row[3], gender=row[4], province=row[5], civilState=row[6])
             listaPersonas.append(persona)
         return listaPersonas
+
+conn = DBConnection()
--- proyecto02/MDI.py
+++ proyecto02/MDI.py
@@ -1,4 +1,6 @@
 #Para que este programa funcione con la base de datos, se necesita instalar el modulo pyodbc que se usa en el archivo DataBaseConnection.py
+#Seguidamente se deberá instalar Access DataBase Engine x64 Redistributable y tener Microsoft Access instalado
+#https://www.microsoft.com/en-us/download/details.aspx?id=54920
 
 
 from WindowCnfg import *
