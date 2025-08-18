ad472cbdc69e66950647bd13cccb4b0f3ea9f2db
Sebas <sebasrojash08@gmail.com>
2025-08-17 20:23:37 -0600

    chore: instructions

--- proyecto02/MDI.py
+++ proyecto02/MDI.py
@@ -1,3 +1,6 @@
+#Para que este programa funcione con la base de datos, se necesita instalar el modulo pyodbc que se usa en el archivo DataBaseConnection.py
+
+
 from WindowCnfg import *
 
 if __name__ == "__main__":
