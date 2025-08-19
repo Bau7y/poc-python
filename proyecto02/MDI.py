403859bb0b2339ec1d4ebbb5d06f56203e1eb860
Sebas <sebasrojash08@gmail.com>
2025-08-19 12:53:51 -0600

    feat: NewP add

--- proyecto02/MDI.py
+++ proyecto02/MDI.py
@@ -1,10 +1,20 @@
 #Para que este programa funcione con la base de datos, se necesita instalar el modulo pyodbc que se usa en el archivo DataBaseConnection.py
 #Seguidamente se deberá instalar Access DataBase Engine x64 Redistributable y tener Microsoft Access instalado
 #https://www.microsoft.com/en-us/download/details.aspx?id=54920
-
-
+#para que el calendario funcione se debe instalar el módulo tkcalendar 
 from WindowCnfg import *
+from tkinter import messagebox
+
+def newPerson():
+    newPerson = NewPersonWindow()
+    newPerson.grab_set()
+
+def mnuHandler():
+    screen.mnuArchivo.add_command(label="Nueva Persona", underline=0, command=newPerson)
+    screen.mnuArchivo.add_command(label="Salir", command=screen.quit)
+
 
 if __name__ == "__main__":
     screen = PrincipalWindow()
+    mnuHandler()
     mainloop()
